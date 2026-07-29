398f5f7013359e15a3df812b8007cab2568526aa
Eunjae Choi <102835167+eunjaech@users.noreply.github.com>
2026-07-29 19:11:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.46 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(maps):
+    answer = 0
+    rows, cols = len(maps), len(maps[0])
+    queue = deque([(0,0)])
+    
+    while queue:
+        r,c = queue.popleft()
+        for dr,dc in [(-1,0),(0,-1), (1,0), (0,1)]:
+            nr, nc = r + dr, c + dc
+            if 0<=nr<rows and 0<=nc<cols and maps[nr][nc] == 1:
+                maps[nr][nc] = maps[r][c]+1
+                queue.append((nr,nc))
+    answer = maps[rows-1][cols-1]
+        
+    return answer if answer > 1 else -1
