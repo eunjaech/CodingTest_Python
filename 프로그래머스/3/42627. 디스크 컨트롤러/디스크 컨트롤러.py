b9835279efb23690ed70d43a5edfd42a75fbb033
Eunjae Choi <102835167+eunjaech@users.noreply.github.com>
2026-08-11 10:20:19 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.36 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,22 +1,24 @@
 import heapq
 def solution(jobs):
     heap = []
-    jobs.sort(key=lambda x: (x[0]) )
-    curr,total,count = 0,0,0
+    jobs.sort(key=lambda x: x[0])
+    current, total, count = 0,0,0
     i = 0
     n = len(jobs)
     
     while count < n:
-        while i < n and jobs[i][0] <= curr:
-            heapq.heappush(heap, [jobs[i][1], jobs[i][0]])
+        # 현재 시각 이하에 요청된 모든 작업을 힙에 추가
+        while i < n and jobs[i][0] <= current:
+            heapq.heappush(heap,[jobs[i][1],jobs[i][0]])
             i+=1
         if heap:
+            #대기 중인 작업이 있으면 소요 시간이 가장 짧은 것부터 처리
             duration, request_time = heapq.heappop(heap)
-            curr += duration
-            total += (curr - request_time)
+            current += duration
+            total += (current-request_time)
             count+=1
-        else:
-            curr = jobs[i][0]
             
-        
+        else:
+            #대기 중인 작업이 없으면 디스크가 쉼 -> 다음 작업의 요청 시점으로 이동
+            current = jobs[i][0]
     return total // n
