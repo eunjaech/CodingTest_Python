bf8dfa18e4eb4e8c1b9df8621bb56f3d5c35f04a
Eunjae Choi <102835167+eunjaech@users.noreply.github.com>
2026-08-13 10:46:11 +0900

    [level 3] Title: 등굣길, Time: 1.30 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,18 @@
+def solution(m, n, puddles):
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    
+    puddle_set = set((p[1],p[0]) for p in puddles)
+    
+    dp[1][1] = 1
+    
+    for r in range(1,n+1):
+        for c in range(1,m+1):
+            if r == 1 and c == 1:
+                continue
+            if (r,c) in puddle_set:
+                dp[r][c] = 0
+            else:
+                dp[r][c] = (dp[r-1][c]+dp[r][c-1]) % 1000000007
+                
+        
+    return dp[n][m]
